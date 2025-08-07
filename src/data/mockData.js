eb610e5e7ae7a298344ce3847d2177c2a3068d6e
AndriiSerkhovetcDev <andrii.serkhovetc.dev@gmail.com>
2025-08-07 14:51:31 +0300

    search functionality by title

---
+++ src/data/mockData.js
@@ -0,0 +1,59 @@
+export const headerProps = {
+	isLogin: false
+}
+export const movieGenresProps = {
+	genres : ["Drama", "Thriller", "Supernatural"]
+}
+
+export const movieInfoProps = {
+	title: "Stranger Things",
+	year: 2019,
+	director: "Shawn Levy",
+	seasons: "3 (5 Episodes)",
+	descriptions: "In 1980s Indiana, a group of young friends witness supernatural forces and secret government exploits. As they search for answers, the children unravel a series of extraordinary mysteries."
+}
+
+export const movieRatingProps = {
+	rating: 3
+}
+
+export const popularSliderProps = {
+	title: "popular this week"
+}
+
+export const movieAgeLimitProps = {
+	limit: '16+'
+}
+
+export const netflixTitles = [
+  "Stranger Things",
+  "The Witcher",
+  "Bird Box",
+  "The Gray Man",
+  "Extraction",
+  "Red Notice",
+  "Don't Look Up",
+  "The Umbrella Academy",
+  "Squid Game",
+  "You",
+  "Money Heist",
+  "The Crown",
+  "Black Mirror",
+  "The Queen’s Gambit",
+  "13 Reasons Why",
+  "The Irishman",
+  "Marriage Story",
+  "Enola Holmes",
+  "The Old Guard",
+  "Army of the Dead",
+  "Outer Banks",
+  "Narcos",
+  "Shadow and Bone",
+  "Ozark",
+  "The Lincoln Lawyer",
+  "All Quiet on the Western Front",
+  "Glass Onion: A Knives Out Mystery",
+  "The Perfect Date",
+  "To All the Boys I've Loved Before",
+  "The Kissing Booth"
+];
