4df001952632229d04e7fe52ed96ff26d4118a44
AndriiSerkhovetcDev <andrii.serkhovetc.dev@gmail.com>
2025-08-14 14:52:01 +0300

    added useEffect for show/hide search input and added debounce timer

--- src/components/HeaderSearch/HeaderSearch.jsx
+++ src/components/HeaderSearch/HeaderSearch.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { netflixTitles } from "../../data/mockData";
 import style from "./HeaderSearch.module.css"
 
 const HeaderSearch = () => {
     const [showSearchInput, setShowSearchInput] = useState(false)
     const [searchValue, setSearchValue] = useState('')
+    const [debouncedSearchValue, setDebouncedSearchValue] = useState('')
     const [isValidValue, setIsValidValue] = useState(false)
     const [filteredMovies, setFilteredMovies] = useState([])
 
     const handlerShowSearchInput = () => {
         setShowSearchInput(showSearchInput => !showSearchInput)
     }
+
+    useEffect(() => {
+       if (showSearchInput) {
+            console.log("search input is show")
+       } else {
+            console.log('search input was clear and hide')
+            handleClearInput();
+            setFilteredMovies([])
+       }
+    }, [showSearchInput])
 
     const hasValidLength = (value) =>  value.length > 2;
     
@@ -19,21 +30,34 @@ const HeaderSearch = () => {
             title.toLowerCase().includes(inputValue.toLowerCase()))
     }
 
-    const handleClear = () => {
+    const handleClearInput = () => {
         setSearchValue("")
         setIsValidValue(false)
     } 
 
     const handlerValidSearchValue = (e) => {
-        const inputValue = e.target.value;
-        setSearchValue(inputValue)
-
-        const isValidLength = hasValidLength(inputValue);
-        setIsValidValue(isValidLength);
-
-        setFilteredMovies(isValidLength ? findMovieBySearch(inputValue) : []);
-    
+        setSearchValue(e.target.value)
     }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedSearchValue(searchValue)
+        }, 300)
+
+        return () => clearTimeout(timer)
+    }, [searchValue])
+
+    useEffect(() => {
+        const isValidLength = hasValidLength(debouncedSearchValue)
+
+        if (isValidLength) {
+            setIsValidValue(true)
+            setFilteredMovies(findMovieBySearch(debouncedSearchValue))
+        } else {
+            setIsValidValue(false)
+            setFilteredMovies([])
+        }
+    },[debouncedSearchValue])
 
     return (
         <div className={style.headerSearchContainer}>
@@ -46,16 +70,16 @@ const HeaderSearch = () => {
                     onChange={handlerValidSearchValue}
                 />
                 {showSearchInput && searchValue && (
-                    <button className={style.resetSearchValue} onClick={handleClear}>×</button>
+                    <button className={style.resetSearchValue} onClick={handleClearInput}>×</button>
                 )}
                 <div className={`${style.searchResultsWrapper} ${showSearchInput && isValidValue ? style.showSearchResult : ''}`}>
                     <span className={style.userSearchResult}> User find: {searchValue}</span>
                     {filteredMovies.length ? (
                         <ul className={style.searchResultsList}>
                             {filteredMovies.map((movie, index) => (
-                            <li key={index} className={style.searchResultItem}>{movie}</li>
-                        ))}
-                    </ul>
+                                <li key={index} className={style.searchResultItem}>{movie}</li>
+                            ))}
+                        </ul>
                     ) : (
                         <p className={style.emptySearhResult}>Movie not found</p>
                     )}
